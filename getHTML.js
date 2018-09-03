e00bdb79bbd25d8e4b5aad4c5c07c7bcbaabfce9
Max <MaxHalleran@gmail.com>
2018-09-03 18:59:34 +0000

    reworked the https request to use a callback function, thereby increasing its modularity

---
+++ getHTML.js
@@ -0,0 +1,36 @@
+const https = require('https');
+
+function getHTML (options, callback) {
+
+  //The get method recieves and sends out the options and then handles the response
+  https.get(options, function (response) {
+
+    //set encoding
+    response.setEncoding('utf-8');
+
+    //initialize a buffer
+    let buf = [];
+
+    //the callback is invoked when the data data is recieved
+    response.on('data', function (data) {
+      buf += data;
+    });
+
+    //When all of the info has been recieved
+    response.on('end', function() {
+      callback(buf);
+    });
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
